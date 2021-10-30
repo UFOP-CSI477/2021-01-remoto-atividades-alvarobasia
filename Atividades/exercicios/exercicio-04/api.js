76a92f937368e4a3cf74674b13b1dbd8b9f79d26
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-10-30 10:19:35 -0300

    remover console.log

--- Atividades/exercicios/exercicio-04/api.js
+++ Atividades/exercicios/exercicio-04/api.js
@@ -2,13 +2,11 @@
 
 function getCEP() {
   const cep = document.getElementById("CEP").value;
-  console.log(cep);
   const url = `https://viacep.com.br/ws/${cep}/json/`;
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       document.getElementById("cep").innerHTML = data.cep;
       document.getElementById("log").innerHTML = data.logradouro;
       document.getElementById("bairro").innerHTML = data.bairro;
